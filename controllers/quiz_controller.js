3794d9e44e7bfeb99a6cc1494aba7b84f6342c36
Patricia C <patriciacastilloperez@yahoo.es>
2015-08-02 17:33:34 +0200

    Agregar nuevas preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -41,5 +41,24 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
   }
+  console.log("res"+resultado)
   res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+};
+
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build(
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
+
+  res.render('quizes/new', {quiz: quiz});
+};
+
+exports.create = function(req, res) {
+
+  var quiz = models.Quiz.build( req.body.quiz );
+
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+    res.redirect('/quizes');  
+  })   // res.redirect: Redirección HTTP a lista de preguntas
 };
