56b81a15c0aec93ecc261aecd8c1cac0e8a02906
Patricia C <patriciacastilloperez@yahoo.es>
2015-08-02 12:18:59 +0200

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,13 @@
 var models = require('../models/models.js');
 
 exports.question = function(req, res){
-	models.Quiz.findAll().success(function(quiz){
+	models.Quiz.findAll().then(function(quiz){
 		res.render('quizes/questions', {pregunta: quiz[0].pregunta});
 	})
 };
 
 exports.answer = function(req, res){
-	models.Quiz.findAll().success(function(quiz){
+	models.Quiz.findAll().then(function(quiz){
 		if(req.query.respuesta === quiz[0].respuesta) {
 			res.render('quizes/answers', {respuesta: 'Correcto'});
 		} else {
