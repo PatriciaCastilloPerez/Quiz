55d97448461b02c982f1194a48e732ef734bbc82
Patricia C <patriciacastilloperez@yahoo.es>
2015-08-20 22:46:41 +0200

    Autenticacion y sesion

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 // Página de entrada (home page)
 router.get('/', function(req, res) {
@@ -15,6 +16,11 @@ router.get('/author', function(req, res) {
 
 router.param('quizId', quizController.load);
 router.param('commentId', commentController.load);
+
+
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
